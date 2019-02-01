28285b5a9b38f48fd457186400e5664f7668fafc
Ilia Rushkin <ilia_rushkin@harvard.edu>
2019-02-01 11:22:43 -0500

    Update R/similR.R

--- R/similR.R
+++ R/similR.R
@@ -13,7 +13,7 @@
 #' @param ik initial number of clusters (the final one will be greater or equal than this)
 #' @param clustering_algorithm as implemented in kmeans(), but note that 'Hartigan-Wong' fails for large data, so here we made 'MacQueen' the default.
 #' @param clustering_itermax as implemented in kmeans()
-#' @param similarity_method as implemented in quanteda::textstat_simil()
+#' @param similarity_method as implemented in quanteda::textstat_simil(), plus an extra method 'jsd' for Jensen-Shannon divergence.
 #' @param keep_vec  whether to return the matrix of the word-vectors
 #'
 #' @return depending on keep_vec, either just the simil matrix/object, or a list with that and the matrix of the word-vectors
@@ -23,7 +23,7 @@
 similR=function(toks, vec=NULL, window_weights=1/(1:5), word_vectors_size=30, x_max=10, n_iter=30, ik=100,
                 clustering_algorithm=c("MacQueen", "Hartigan-Wong", "Lloyd", "Forgy"),
                 clustering_itermax=1000,
-                similarity_method='cosine',
+                similarity_method=c("jsd", "correlation", "cosine", "jaccard", "ejaccard", "dice", "edice", "simple matching", "hamann", "faith"),
                 keep_vec=FALSE
 
                 ){
@@ -46,8 +46,70 @@ similR=function(toks, vec=NULL, window_weights=1/(1:5), word_vectors_size=30, x_
   colnames(dfm)=hotspots
   dfm=quanteda::dfm_compress(dfm)
   dfm=quanteda::dfm_weight(dfm,scheme='prop')
-  simmat=quanteda::textstat_simil(dfm,method=similarity_method)
+  if(similarity_method[1]=='jsd'){
+    simmat=1-dfm%>%as.matrix()%>%proxy::dist(method=jsd)
+  }else{
+    simmat=quanteda::textstat_simil(dfm,method=similarity_method[1])
+  }
   cat('Done in', round(proc.time()[3]-tic),'sec.\n')
   if(keep_vec) return(list(simmat=simmat,vec=vec)) else return(simmat)
+
+}
+
+
+
+
+
+
+
+
+
+#' Jensen-Shannon divergence of discrete probability distibutions JSD(p||q)
+#'
+#' @param p first probability distribution
+#' @param q second probability distribution
+#'
+#' @return JSD(p||q), a numeric value
+#' @export
+#' @details Assumes that both p and q are normalized (sum(p)=sum(q)=1) and that they contain no NA values. Calculated with base-2 log, so the bounds are [0,1].
+jsd=function(p,q){
+
+  # p=p/sum(p)
+  # q=q/sum(q)
+
+  m=0.5*(p+q)
+
+  x=log(m/p)
+  x[is.infinite(x)]=0
+  x[is.na(x)]=0
+
+  y=log(m/q)
+  y[is.infinite(y)]=0
+  y[is.na(y)]=0
+
+  #Divide by log(2) so to normalize to the interval
+  return(-0.5*(sum(p*x)+sum(q*y))/log(2))
+
+}
+
+#' Kullback-Leibler divergence between discrete probability distributions KLD(p||q)
+#'
+#' @param p first probability distribution ("to which"), a numeric vector, no NAs
+#' @param q second probability distribution ("from which"), a numeric vector of same length as \code{p}, no NAs
+#'
+#' @return KLD(p||q), a numeric value
+#' @export
+#' @details Assumes that both p and q are normalized (sum(p)=sum(q)=1). Note that by definition the Kullback-Leibler divergence is asymmetric: KLD(p||q) is not equal to KLD(q||p).
+kld=function(p,q){
+
+  # p=p/sum(p)
+  # q=q/sum(q)
+
+  z=log(q/p)
+  z[is.infinite(z)]=0
+  z[is.na(z)]=0
+
+  return(-sum(p*z))
+
 
 }
