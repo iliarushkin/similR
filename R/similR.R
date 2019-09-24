7a6d7f02538a40a40a87da72cbf347e9844189a3
Ilia Rushkin <ilia_rushkin@harvard.edu>
2019-09-24 13:21:51 -0400

    update

--- R/similR.R
+++ R/similR.R
@@ -4,13 +4,15 @@
 #'
 #' Document similarity based on word vectorization
 #'
-#' @param toks tokens representing a document corpus
+#' @param toks1 tokens from a document corpus
+#' @param toks2 tokens from another document corpus (if NULL, assumes toks2=toks1)
 #' @param vec if NULL, will calculate the vector embedding of words. If this lengthy calculation needs to be skipped - pass the prepared matrix of vectors here (for instance, taken from the previous run of this function with keep_vec=TRUE).
 #' @param window_weights weights of the window to use for co-occurrence of tokens.
 #' @param word_vectors_size dimensionality of vector space
 #' @param x_max max number of co-occurrences to use in the weighting function
 #' @param n_iter number of GloVe iterations
-#' @param ik initial number of clusters (the final one will be greater or equal than this)
+#' @param ik initial number of clusters (if soft_ik is TRUE, the final one will be greater or equal than this)
+#' @param soft_ik whether to use xmeans instead of kmeans for hotspots
 #' @param clustering_algorithm as implemented in kmeans(), but note that 'Hartigan-Wong' fails for large data, so here we made 'MacQueen' the default.
 #' @param clustering_itermax as implemented in kmeans()
 #' @param similarity_method as implemented in quanteda::textstat_simil(), plus an extra method 'jsd' for Jensen-Shannon divergence.
@@ -20,7 +22,8 @@
 #' @export
 #'
 #' @examples
-similR=function(toks, vec=NULL, hotspots=NULL, window_weights=1/(1:5), word_vectors_size=300, x_max=10, n_iter=30, ik=100,
+similR=function(toks1, toks2, vec=NULL, hotspots=NULL, window_weights=1/(1:5), word_vectors_size=300, x_max=10, n_iter=30, ik=100,
+                soft_ik=FALSE,
                 clustering_algorithm=c("MacQueen", "Hartigan-Wong", "Lloyd", "Forgy"),
                 clustering_itermax=1000,
                 similarity_method=c("cosine", "jaccard", "ejaccard", "dice", "edice", "simple matching", "hamann", "faith", "correlation", "jsd"),
@@ -30,7 +33,14 @@ similR=function(toks, vec=NULL, hotspots=NULL, window_weights=1/(1:5), word_vect
                 ){
 
   tic=proc.time()[3]
-  cooc=quanteda::fcm(toks,context='window', window=length(window_weights), weights=window_weights, count='weighted')
+
+  if(!is.null(toks2)){
+    quanteda::docnames(toks1)=paste0('IDtoks1_', quanteda::docnames(toks1))
+    quanteda::docnames(toks2)=paste0('IDtoks2_', quanteda::docnames(toks2))
+    toks1=add_tokens_with_empties(toks1,toks2)
+  }
+
+  cooc=quanteda::fcm(toks1,context='window', window=length(window_weights), weights=window_weights, count='weighted')
 
   if(is.null(hotspots)){
     #Vectorize using text2vec
@@ -42,22 +52,33 @@ similR=function(toks, vec=NULL, hotspots=NULL, window_weights=1/(1:5), word_vect
     }
 
     cat('Computing hotspots...\n')
-    hotspots=xmeans(vec, ik=ik, algorithm=clustering_algorithm[1], iter.max = clustering_itermax)$cluster
-  }
-  dfm=quanteda::dfm(quanteda::tokens_select(toks,featnames(cooc)))
-  colnames(dfm)=hotspots
-  dfm=quanteda::dfm_compress(dfm)
-  dfm=quanteda::dfm_weight(dfm,scheme='prop')
-  cat('Similarity method:', similarity_method[1],'\n')
-  if(similarity_method[1]=='jsd'){
-    simmat=1-(dfm%>%as.matrix()%>%proxy::dist(method=jsd))/log(2)
-  }else{
-    if(packageVersion('quanteda')=='1.4.0'){
-      simmat=quanteda::textstat_simil_old(dfm,method=similarity_method[1])
+    if(soft_ik){
+      hotspots=xmeans(vec, ik=ik, algorithm=clustering_algorithm[1], iter.max = clustering_itermax)$cluster
     }else{
-      simmat=quanteda::textstat_simil(dfm,method=similarity_method[1])
+      hotspots=kmeans(vec, centers=ik, algorithm=clustering_algorithm[1], iter.max = clustering_itermax)$cluster
     }
   }
+
+  dfm=quanteda::dfm(quanteda::tokens_select(toks1,quanteda::featnames(cooc)))
+  quanteda::featnames(dfm)=hotspots
+  dfm=quanteda::dfm_compress(dfm)
+  dfm=quanteda::dfm_weight(dfm,scheme='prop')
+
+  cat('Similarity method:', similarity_method[1],'\n')
+  if(is.null(toks2)){
+    simmat=quanteda::textstat_simil(dfm, method=similarity_method[1])
+  }else{
+    dfm2=quanteda::dfm_subset(dfm, grepl('^IDtoks2_',quanteda::docnames(dfm)))
+    dfm=quanteda::dfm_subset(dfm, grepl('^IDtoks1_',quanteda::docnames(dfm)))
+    quanteda::docnames(dfm)=gsub('^IDtoks1_','',quanteda::docnames(dfm))
+    quanteda::docnames(dfm2)=gsub('^IDtoks2_','',quanteda::docnames(dfm2))
+    simmat=quanteda::textstat_proxy(y=dfm, x=dfm2, method=similarity_method[1])%>%t()%>%as.matrix()
+  }
+
+
+  # if(similarity_method[1]=='jsd'){
+    # simmat=1-(dfm%>%as.matrix()%>%proxy::dist(method=jsd))/log(2)
+  # }
   cat('Done in', round(proc.time()[3]-tic),'sec.\n')
   if(keep_hotspots){
     simmat=list(simmat=simmat, hotspots=hotspots)
@@ -70,7 +91,15 @@ similR=function(toks, vec=NULL, hotspots=NULL, window_weights=1/(1:5), word_vect
 }
 
 
+add_tokens_with_empties=function(toks1, toks2){
 
+  toks1=toks1%>%as.list()%>%lapply(function(x){x[x=='']=' '; x})%>%quanteda::as.tokens()
+  toks2=toks2%>%as.list()%>%lapply(function(x){x[x=='']=' '; x})%>%quanteda::as.tokens()
+  attr(toks2,'what')=attr(toks1,'what')
+  (toks1+toks2)%>%quanteda::tokens_remove(' ', padding=TRUE)
+
+
+}
 
 
 
